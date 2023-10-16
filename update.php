fcd7295cc158aca2784821a897d8390ab9872fed
bitDig6 <ummenabilajalal@gmail.com>
2023-10-17 01:36:12 +0600

    Added Update Operation

--- update.php
+++ update.php
@@ -1,6 +1,7 @@
 <?php
 include('conn.php');
-$up = $_POST['check'];
+$update = $_POST['up'];
 //write query for updating status
+$conn->query("UPDATE taskslist SET task_status='Done' WHERE task_no='$update' ");
 header('location:index.php');
 ?>
